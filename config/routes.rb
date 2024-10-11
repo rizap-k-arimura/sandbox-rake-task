f0728a14ede31a4f9302e2e6aedd3de78e45f93a
rizap-k-arimura <kaisei.arimura@rizapgroup.com>
2024-10-11 12:41:10 +0900

    ECSタスク起動機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,10 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/*
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+
+  get "run_task", to: "ecs#run_task"
   
- namespace 'api' do 
+  namespace 'api' do 
     get 'run_example_task', to: 'task#run_example_task'
   end
 
